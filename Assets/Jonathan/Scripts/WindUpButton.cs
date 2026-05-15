7b42830fc9a2908ed34f11ada508c5127794f620
Jappstwr <johnochphil08@gmail.com>
2026-05-15 14:17:22 +0200

    ben2 idk man, fixed camera shie

--- Assets/Jonathan/Scripts/WindUpButton.cs
+++ Assets/Jonathan/Scripts/WindUpButton.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class WindUpButton : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+
+
     void Start()
     {
         
