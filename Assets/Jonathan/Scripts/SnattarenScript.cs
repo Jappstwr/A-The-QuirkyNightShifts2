e65d1d7707c7524dd2cddac698ebc532e128e5f6
Jappstwr <johnochphil08@gmail.com>
2026-05-26 20:53:37 +0200

    snattaren is almost finished
    gotta push, gaio comes to the rescue

--- Assets/Jonathan/Scripts/SnattarenScript.cs
+++ Assets/Jonathan/Scripts/SnattarenScript.cs
@@ -53,7 +53,33 @@ public class SnattarenScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateVisibility();
+        UpdateAI();
+
+        aiTimer += Time.deltaTime;
+
+
+        moveTimer -= Time.deltaTime; 
+
+        if (currentWaypoint.isOffice && !hasAttackStarted)
+        {
+            hasAttackStarted = true; 
+            attacking = true;
+            attackTimer = 6f;
+            retreatTimer = Random.Range(1,3); 
+        }
+        if (attacking)
+        {
+            AnimAttack(); 
+        }
+
+        if (moveTimer <= 0f)
+        {
+            moveTimer = checkInterval;
+
+            OpportunityMovement(); 
+        }
+
     }
 
     public void UpdateAI()
@@ -93,5 +119,88 @@ public class SnattarenScript : MonoBehaviour
         currentRoom = wp.roomIndex;
 
         Debug.Log($"Snattaren moved to {currentRoom}"); 
+    }
+
+    public void OpportunityMovement()
+    {
+        int roll = Random.Range(1,21);
+        
+        if (roll <= aiLvl)
+        {
+            MoveForward(); 
+        }
+    }
+    public void MoveForward()
+    {
+        if (currentPathIndex >= paths.Length - 1)
+        {
+            return;
+        }
+
+        currentPathIndex++;
+
+        MoveToWaypoint(currentPathIndex); 
+    }
+    public void AnimAttack()
+    {
+        attackTimer -= Time.deltaTime;
+        retreatTimer -= Time.deltaTime; 
+
+        if (nightscript._isFlashing && retreatTimer <= 0f)
+        {
+            hasAttackStarted = false;
+            attacking = false; 
+
+            currentPathIndex = 0;
+
+            MoveToWaypoint(currentPathIndex); 
+        }
+        else if (!nightscript._isFlashing)
+        {
+            //take 'em bloody batteries
+        }
+    }
+    public void UpdateVisibility()
+    {
+        if (currentWaypoint == null)
+        {
+            sr.enabled = false;
+            return;
+        }
+
+        if ((currentWaypoint.isHallway || currentWaypoint.isOffice) && nightscript._monitorOpen)
+        {
+            sr.enabled = false;
+            return;
+        }
+
+        if (currentWaypoint.isLeftOffice && nightscript._leftClosed)
+        {
+            sr.enabled = false;
+            return;
+        }
+
+        if (currentWaypoint.isRightOffice && nightscript._rightClosed)
+        {
+            sr.enabled = false;
+            return;
+        }
+
+        if (currentWaypoint.isHallway)
+        {
+            sr.enabled = nightscript._isFlashing;
+            return;
+        }
+
+        if (currentWaypoint.isOffice)
+        {
+            sr.enabled = true;
+            return;
+        }
+
+
+        bool visible = maincamerascript != null && maincamerascript.ActiveCamera == currentRoom && maincamerascript.camerasOpen;
+
+        sr.enabled = visible;
     }
 }
